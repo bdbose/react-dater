a970b45d681779af70dc2193398872a9b6e2c118
bdbose <bdbose123@gmail.com>
2022-02-07 01:38:11 +0530

    Updates

---
+++ precache-manifest.7fd65c0e4fcd635aaa95108eea46427d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1f6ccecab8d92290c9db1797c264f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76ff48891f2a5e969fac",
+    "url": "./static/css/main.ed84e5f0.chunk.css"
+  },
+  {
+    "revision": "5b39a60a6e7af80af3a8",
+    "url": "./static/js/2.b71aed11.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.b71aed11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ff48891f2a5e969fac",
+    "url": "./static/js/main.f6294cc0.chunk.js"
+  },
+  {
+    "revision": "3e688619a88a3460fbdb",
+    "url": "./static/js/runtime-main.60280d04.js"
+  }
+]);
